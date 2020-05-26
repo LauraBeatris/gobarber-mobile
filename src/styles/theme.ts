f25577bc18c5ff45445fd491ae72fad703615411
Laura <laurabeatriserafim@gmail.com>
2020-05-26 07:43:58 -0300

    fix: merge styled components theme declarations

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,13 @@
 const theme = {
   colors: {
-    dark: "#312e38"
+    dark: "#312e38",
+    white: "#f4ede8"
+  },
+  fonts: {
+    medium: 'RobotoSlab-Medium'
   }
 }
+
+export type ThemeTypes = typeof theme;
 
 export default theme;
