df3e1d2c8fefdb87660f5990fdd733bd3a7cf64d
Laura <laurabeatriserafim@gmail.com>
2020-05-30 17:34:07 -0300

    feat: add form to signup screen

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,110 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext, useRef } from 'react';
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/mobile';
+import { useNavigation } from '@react-navigation/native';
+import { ThemeContext } from 'styled-components';
+import Icon from 'react-native-vector-icons/Feather'
 
-const SignUp: React.FC = () => <View></View>;
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import logo from '../../assets/logo.png';
+
+import {
+  Container,
+  Content,
+  Title,
+  SignInButton,
+  SignInButtonText
+} from './styles';
+
+const SignUp: React.FC = () => {
+  const theme = useContext(ThemeContext);
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation();
+
+  const handleSubmit = (): void => {
+    formRef?.current?.submitForm();
+  };
+
+  const handleNavigationToSignIn = (): void => {
+    navigation.goBack();
+  };
+
+  const signUp = (): void => {
+    // TODO - Send account data to API
+  };
+
+  return (
+    <Container>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{flex: 1}}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Content>
+              <Image source={logo} />
+
+              <View>
+                <Title>Create an account</Title>
+              </View>
+
+              <Form
+                ref={formRef}
+                onSubmit={signUp}
+              >
+                <Input
+                  name="name"
+                  icon="user"
+                  placeholder="Full name"
+                  returnKeyType="next"
+                  autoCapitalize="words"
+                  autoCompleteType="name"
+                />
+                <Input
+                  name="email"
+                  icon="mail"
+                  placeholder="Email"
+                  returnKeyType="next"
+                  autoCapitalize="none"
+                  autoCompleteType="email"
+                />
+                <Input
+                  name="password"
+                  icon="lock"
+                  placeholder="Password"
+                  returnKeyType="done"
+                  autoCompleteType="password"
+                  textContentType="newPassword"
+                  secureTextEntry
+                />
+
+                <Button
+                  onPress={handleSubmit}
+                >
+                  Create
+                </Button>
+              </Form>
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <SignInButton onPress={handleNavigationToSignIn}>
+        <Icon
+          name="arrow-left"
+          color={theme.colors.white}
+          size={16}
+        />
+        <SignInButtonText>
+          Already have an account? Login
+        </SignInButtonText>
+      </SignInButton>
+    </Container>
+  )
+};
 
 export default SignUp;
