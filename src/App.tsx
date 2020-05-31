386c28d9ee8f49ab07f2a4e73af2313c830ca8e2
Laura <laurabeatriserafim@gmail.com>
2020-05-31 13:02:14 -0300

    feat: config envs and async storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { ThemeProvider } from 'styled-components'
+import "react-native-gesture-handler";
+import React from "react";
+import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from "styled-components";
 
-import Routes from './router';
-import theme from './styles/theme'
+import Routes from "./router";
+import theme from "./styles/theme";
 
 const App = () => {
   return (
     <NavigationContainer>
       <ThemeProvider theme={theme}>
-          <StatusBar
-            barStyle="light-content"
-            backgroundColor="#312e38"
-          />
-          <Routes />
-        </ThemeProvider>
+        <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+        <Routes />
+      </ThemeProvider>
     </NavigationContainer>
   );
 };
---
+++ src/config/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { API_URL } from "react-native-dotenv";
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+export default api;
