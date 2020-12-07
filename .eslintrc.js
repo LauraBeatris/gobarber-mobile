35e44c6a31dea9f4551b1e0932549dfa05cf7206
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-12-07 18:47:14 -0300

    style: disable explict types definition rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,9 @@ module.exports = {
           "group": "external"
         }
       ]
-    }]
+    }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
   },
   settings: {
     "import/resolver": {
