74afbc1907fb39c6378189a61d19bbb8f76c1db8
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-12-08 09:56:33 -0300

    chore: add dashboard providers list

---
+++ src/pages/Dashboard/DashboardProvidersList/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { FlatList, RefreshControl } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import { keyExtractorId } from "~/constants/flatLists";
+
+import useProviders from "~/hooks/useProviders";
+import useUserAvatarURI from "~/hooks/useUserAvatarURI";
+import useNavigate from "~/hooks/useNavigate";
+import theme from "~/styles/theme";
+import { daysInWeekBusinessIntervalText, hoursInDayBusinessIntervalText } from "~/constants/appointments";
+import { User } from "~/shared/types/apiSchema";
+import { CREATE_APPOINTMENT_ROUTE } from "~/router/routes";
+
+import {
+  Title,
+  Container,
+  ProviderInfo,
+  ProviderName,
+  ProviderAvatar,
+  ProviderSchedule,
+  ProviderContainer,
+  ProviderScheduleText,
+  NoProvidersAvailableText,
+  ProvidersListActivityIndicator,
+} from "./styles";
+import { ProviderItemProps } from "./types";
+
+const ProviderItem: React.FC<ProviderItemProps> = ({ item }) => {
+  const userAvatarURI = useUserAvatarURI(item);
+  const navigate = useNavigate();
+
+  return (
+    <ProviderContainer onPress={navigate(CREATE_APPOINTMENT_ROUTE, { id: item.id })}>
+      <ProviderAvatar source={userAvatarURI} />
+
+      <ProviderInfo>
+        <ProviderName>{item.name}</ProviderName>
+
+        <ProviderSchedule>
+          <Icon
+            name="calendar"
+            color={theme.colors.yellow}
+            size={15}
+          />
+
+          <ProviderScheduleText>
+            {daysInWeekBusinessIntervalText}
+          </ProviderScheduleText>
+        </ProviderSchedule>
+
+        <ProviderSchedule>
+          <Icon
+            name="clock"
+            color={theme.colors.yellow}
+            size={15}
+          />
+
+          <ProviderScheduleText>
+            {hoursInDayBusinessIntervalText}
+          </ProviderScheduleText>
+        </ProviderSchedule>
+      </ProviderInfo>
+    </ProviderContainer>
+  );
+};
+
+const DashboardProvidersList: React.FC = () => {
+  const { providers, loading, fetchProviders } = useProviders();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleRefresh = () => {
+    setRefreshing(true);
+
+    fetchProviders()
+      .finally(() => setRefreshing(false));
+  };
+
+  return (
+    <Container>
+      <Title>Cabeleireiros</Title>
+
+      {
+        loading ? (
+          <ProvidersListActivityIndicator size="large" />
+        ) : (
+          <FlatList<User>
+            data={providers}
+            refreshControl={(
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={handleRefresh}
+                tintColor={theme.colors.white}
+              />
+            )}
+            renderItem={({ item }) => <ProviderItem item={item} />}
+            keyExtractor={keyExtractorId}
+            ListEmptyComponent={(
+              <NoProvidersAvailableText>
+                No providers available
+              </NoProvidersAvailableText>
+            )}
+          />
+        )
+      }
+    </Container>
+  );
+};
+
+export default DashboardProvidersList;
---
+++ src/pages/Dashboard/DashboardProvidersList/types.ts
@@ -0,0 +1,5 @@
+import { User } from "~/shared/types/apiSchema";
+
+export interface ProviderItemProps {
+  item: User;
+}
