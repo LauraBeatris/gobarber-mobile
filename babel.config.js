0845c691a35ce6ecb04f39f99c4f3f4b4d4ef7b0
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-18 19:16:10 -0300

    fix: add babel-plugin-root-import to fix absolute import paths resolution

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,13 @@ module.exports = {
     'module:metro-react-native-babel-preset',
     'module:react-native-dotenv'
   ],
+  plugins: [
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathPrefix: '~',
+        rootPathSuffix: 'src',
+      },
+    ],
+  ]
 };
