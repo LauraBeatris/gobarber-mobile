fe47dd92b2391537f3d219a2bc43524f383865b8
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-01-18 19:17:47 -0300

    chore: implement header at create appointment screen

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/Feather";
 
-const CreateAppointment: React.FC = () => <></>;
+import Header from "~/components/Header";
+import theme from "~/styles/theme";
+
+import { Container, CreateAppointmentHeaderText } from "./styles";
+
+const CreateAppointment: React.FC = () => {
+  const { goBack } = useNavigation();
+
+  return (
+    <Container>
+      <Header>
+        <TouchableOpacity onPress={goBack}>
+          <Icon
+            name="chevron-left"
+            size={24}
+            color={theme.colors.gray}
+          />
+
+        </TouchableOpacity>
+
+        <CreateAppointmentHeaderText>
+          Agendamento
+        </CreateAppointmentHeaderText>
+      </Header>
+    </Container>
+  );
+};
 
 export default CreateAppointment;
---
+++ src/pages/CreateAppointment/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const CreateAppointmentHeaderText = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 20px;
+  font-family: 'RobotoSlab-Medium';
+`;
