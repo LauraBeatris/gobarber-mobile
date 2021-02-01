66fcc6e4fdf50879c68de609257eeaf750178a72
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-02-01 11:37:05 -0300

    chore: add title styled component

--- src/styles/components.ts
+++ src/styles/components.ts
@@ -4,3 +4,10 @@ export const ScreenContainer = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.dark};
 `;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 25px;
+  font-family: 'RobotoSlab-Medium';
+  margin-bottom: 24px;
+`;
