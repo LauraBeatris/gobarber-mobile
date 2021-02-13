99372f1ac06f2182f584d264095a62921dc12342
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-02-13 14:44:59 -0300

    fix(android): apply a higher padding top to android only

--- src/components/Layout/Header/HeaderContainer/styles.ts
+++ src/components/Layout/Header/HeaderContainer/styles.ts
@@ -1,11 +1,20 @@
-import styled from "styled-components/native";
+import { Platform } from "react-native";
+import styled, { css } from "styled-components/native";
 
 import { HeaderContainerProps } from "./types";
 
 export const HeaderContainerView = styled.View<HeaderContainerProps>`
   width: 100%;
-  padding: 24px;
-  flex-direction: row;
   align-items: center;
+  flex-direction: row;
   justify-content: space-between;
+
+  ${Platform.select({
+    ios: css`
+      padding: 24px;
+    `,
+    android: css`
+      padding: 48px 24px 24px;
+    `,
+  })}
 `;
